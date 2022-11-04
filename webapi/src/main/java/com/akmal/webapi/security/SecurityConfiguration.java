e9b4c78c90439e30a74e70aa393806857e2223da
Nur Akmal Jalil <nurakmaljalil91@gmail.com>
2022-11-05 00:13:33 +0800

    Add jwt authentication

--- webapi/src/main/java/com/akmal/webapi/security/SecurityConfiguration.java
+++ webapi/src/main/java/com/akmal/webapi/security/SecurityConfiguration.java
@@ -1,11 +1,19 @@
 package com.akmal.webapi.security;
 
+import com.akmal.webapi.filter.CustomAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 public class SecurityConfiguration {
@@ -18,6 +26,23 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        var customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager(new AuthenticationConfiguration()));
+        customAuthenticationFilter.setFilterProcessesUrl("/api/v1/login");
+        http.cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers("/api/v1/login/**").permitAll().and()
+                .authorizeRequests().antMatchers(GET, "/api/v1/users/**").hasAnyAuthority("USER_ROLE").and()
+                .authorizeRequests().antMatchers(POST, "/api/v1/users/**").hasAnyAuthority("ADMIN_ROLE").and()
+                .authorizeRequests().anyRequest().authenticated().and()
+                .addFilter(customAuthenticationFilter);
+
+        return http.build();
     }
 
 }
