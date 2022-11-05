77fd820b6c143483e4640a7b0966673e94f0a97e
Nur Akmal Jalil <nurakmaljalil91@gmail.com>
2022-11-06 02:25:08 +0800

    Add simple login

--- webapp/src/stores/authentication.ts
+++ webapp/src/stores/authentication.ts
@@ -1,11 +1,18 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { reactive } from "vue";
+import { useRouter } from 'vue-router'
 import axios from "axios";
 import type { LoginResponse } from "@/types/LoginResponse";
 import type { LoginForm } from "@/types/LoginForm";
 
 export const useAuthStore = defineStore("authentication", () => {
-  const user = ref(null);
+  const router = useRouter();
+  let user = reactive<LoginResponse>({
+    username: "",
+    roles: [],
+    accessToken: "",
+    refreshToken: "",
+  });
   function login(loginForm: LoginForm) {
     const formData = new FormData();
     formData.set("username", loginForm.username);
@@ -17,7 +24,9 @@ export const useAuthStore = defineStore("authentication", () => {
         },
       })
       .then(function (response) {
-        console.log(response);
+        user = response.data;
+        localStorage.setItem("user", JSON.stringify(user));
+        router.push("/");
       });
   }
 
